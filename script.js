f7ecd7fcace8a5afa19ca9d91175f052579a07ab
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-29 16:46:53 +0100

    Point 1. Placeholder.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // restore the html button in the variable "numberInput" to have the placeholder
 // "possible number"
 console.dir(numberInput);
+numberInput.placeholder="possible number";
 
 // restore the html element in the variable "calculateHighestSalaries" to have the button text
 // "Calculate highest salary"
